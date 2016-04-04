fe09840af4a2dee7bcc2a5bf0e1f1b4aeef87617
Robert Osborne <robert@wodenwebdevelopment.com>
2016-04-04 19:26:00 -0400

    Fix search by email address in admin interface

--- nopassword/admin.py
+++ nopassword/admin.py
@@ -4,6 +4,6 @@ from .models import LoginCode
 
 class LoginCodeAdmin(admin.ModelAdmin):
     list_display = ( 'user', 'code', 'timestamp', 'next',)
-    search_fields = ('user',)
+    search_fields = ('user__email',)
 
 admin.site.register(LoginCode, LoginCodeAdmin)
